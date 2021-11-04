dc5c2d1e15d482d0d11401153aa839644c0217c0
xjboss <9338143+xjboss@users.noreply.github.com>
2021-11-04 15:30:02 +0800

    update

--- install-amglogic-mirror.sh
+++ install-amglogic-mirror.sh
@@ -7,6 +7,7 @@
 url_clone="";
 
 url_raw="";
+url_raw2="";
 
 clone_mirror(){
 	printf "Choose github clone mirror\n
@@ -44,14 +45,23 @@ raw_mirror(){
 	while [ $tryagain -eq 1 ]; do
 		$capture = 1;
 		case $capture in 
-		0) url_raw="https:\\/\\/raw.githubusercontent.com\\/";;
-		1) url_raw="https:\\/\\/raw.fastgit.org\\/";;
-		2) url_raw="https:\\/\\/cdn.staticaly.com\\/gh\\/";;
-		3) url_raw="https:\\/\\/ghproxy.com\\/https:\\/\\/raw.githubusercontent.com\\/";;
+		0) url_raw="https:\\/\\/raw.githubusercontent.com\\/";url_raw2="https://raw.githubusercontent.com/";;
+		1) url_raw="https:\\/\\/raw.fastgit.org\\/";url_raw2="https://raw.fastgit.org/";;
+		2) url_raw="https:\\/\\/cdn.staticaly.com\\/gh\\/";url_raw2="https://cdn.staticaly.com/gh/";;
+		3) url_raw="https:\\/\\/ghproxy.com\\/https:\\/\\/raw.githubusercontent.com\\/";url_raw2="https://ghproxy.com/https://raw.githubusercontent.com/";;
 		*) printf "\nTry again.\n"; tryagain=1;;
 		esac
 		echo
-		echo "Github raw URL -> $url_clone"
+		echo "Github raw URL -> $url_raw"
 		echo
 	done
 }
+
+clone_mirror
+raw_mirror
+
+cd /tmp
+
+wget "${url_raw2}xe5700/kvmd-armbian/master/install-amglogic.sh" -o install.sh
+
+sed "s/https:\\/\\/raw.githubusercontent.com\\//$url_raw/" | sed "s/https:\\/\\/github.com\\//$url_clone/"
